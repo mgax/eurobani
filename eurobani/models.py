c3f115e1e776133965861cd47712f5bcd4456fcf
Alex Morega <alex@grep.ro>
2016-10-20 17:13:43 +0300

    models for Contract and Payment

--- eurobani/models.py
+++ eurobani/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+
+class Contract(models.Model):
+    data = JSONField()
+
+class Payment(models.Model):
+    data = JSONField()
