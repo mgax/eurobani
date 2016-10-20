aa50db12eb9f3c0df8fc4c16c108091bb0c6e472
Alex Morega <alex@grep.ro>
2016-10-20 17:07:36 +0300

    barebones django app and project

---
+++ eurobani/site/example_settings_local.py
@@ -0,0 +1,2 @@
+SECRET_KEY = 'TODO replace with something random'
+DEBUG = True
---
+++ eurobani/site/settings_common.py
@@ -0,0 +1,16 @@
+INSTALLED_APPS = [
+    'eurobani',
+]
+
+MIDDLEWARE = [
+]
+
+ROOT_URLCONF = 'eurobani.site.urls'
+WSGI_APPLICATION = 'eurobani.site.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = False
+USE_L10N = False
+USE_TZ = True
+STATIC_URL = '/static/'
